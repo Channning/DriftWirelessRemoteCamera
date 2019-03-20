0e5d5c96f46f3afc0e26ee59b5bee960fe7cb03d
rongbh <channing.rong@gmail.com>
2019-03-20 10:29:39 +0800

    Initial Commit

---
+++ DriftWirelessRemoteControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriftWirelessRemoteControl
+//
+//  Created by Channing_rong on 2019/3/20.
+//  Copyright © 2019 Channing.rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriftWirelessRemoteControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DriftWirelessRemoteControl
+//
+//  Created by Channing_rong on 2019/3/20.
+//  Copyright © 2019 Channing.rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
